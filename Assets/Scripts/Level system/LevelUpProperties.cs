268e4158ce1933de1712d91d18a6b082e8995d04
Philip Ullersted <philipullersted@hotmail.com>
2016-03-25 10:51:49 +0100

    Levelup system started

---
+++ Assets/Scripts/Level system/LevelUpProperties.cs
@@ -0,0 +1,9 @@
+﻿namespace Assets.Scripts.Level_system
+{
+    [System.Serializable]
+    public class LevelUpProperties
+    {
+        public int XpToLevel;
+        public float Damage;
+    }
+}

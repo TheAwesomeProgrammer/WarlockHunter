d5fe7dfbbe471908dd5a43f4bec97c42a95e2f42
Philip Ullersted <philipullersted@hotmail.com>
2016-03-26 22:43:13 +0100

    A litte potion stuff and maybe prefab fix

---
+++ Assets/Scripts/Player/Alchymist/DecreaseSpeed.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class DecreaseSpeed : MonoBehaviour
+{
+    public float ProcentDecrease;
+
+    private Shot _shot;
+
+    void Start()
+    {
+        _shot = GetComponent<Shot>();
+        ProcentDecrease /= 100;
+    }
+
+    void Update()
+    {
+        _shot.Speed -= (_shot.Speed*ProcentDecrease);
+    }
+}
+
+   

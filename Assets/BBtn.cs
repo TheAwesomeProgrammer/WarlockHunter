05a433ad02b1fa1be670d0d38fe61f8ef7967b02
Kasper Nielsen <kallenielsen87@live.dk>
2016-03-26 13:03:14 +0100

    Cooldown indicaters for button A, B and X added.

---
+++ Assets/BBtn.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class BBtn : MonoBehaviour {
+
+    public Image Bbtn;
+    public float Cd = 60f;
+    public float currentCd;
+    public bool DecreaseCD;
+
+    GameObject TextBBtn;
+    Text ourBBtn;
+
+	void Start ()
+    {
+        TextBBtn = GameObject.Find("CdBText");
+        ourBBtn = TextBBtn.GetComponent<Text>();
+	}
+	
+	void Update ()
+    {
+
+        if (Input.GetKeyDown(KeyCode.S) && !DecreaseCD)
+        {
+            // Place ward 
+            Bbtn.fillAmount = 360;
+            currentCd = Cd;
+            DecreaseCD = true;
+        }
+
+        if(DecreaseCD)
+        {
+            Bbtn.fillAmount -= 1f / Cd * Time.deltaTime; 
+            currentCd -= Time.deltaTime;
+            ourBBtn.text = currentCd.ToString("F0");
+
+            if(currentCd <= 0)
+            {   
+                DecreaseCD = false;
+            }
+        }
+
+        else
+        {
+            
+            Bbtn.fillAmount = 0;
+            ourBBtn.text = "";
+            Cd = 60;
+        }
+    }
+}
--- Assets/Scripts/Extensions/StaticClasses/Timer.cs
+++ Assets/Scripts/Extensions/StaticClasses/Timer.cs
@@ -6,6 +6,7 @@ public class CallbackInfo
 {
     public GameObject ObjectToCall;
     public string MethodName;
+
     
     public CallbackInfo(GameObject pObjectToCall,string pMethodName)
     {
